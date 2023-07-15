b918f3b17c50bd0bfee947d45df381dd330337d0
nowfuture011130 <105948127+nowfuture011130@users.noreply.github.com>
2023-07-14 22:14:58 -0400

    update

--- src/screens/OrderLiveUpdates/index.js
+++ src/screens/OrderLiveUpdates/index.js
@@ -3,8 +3,10 @@ import React, { useEffect, useState, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { DataStore } from "@aws-amplify/datastore";
 import { Order, Driver } from "../../models";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome5, MaterialIcons } from "@expo/vector-icons";
+import { useAuthContext } from "../../contexts/AuthContext";
 const OrderLiveUpdates = ({ id }) => {
+  const { dbUser } = useAuthContext();
   const [order, setOrder] = useState(null);
   const [driver, setDriver] = useState(null);
   const mapRef = useRef(null);
@@ -20,11 +22,25 @@ const OrderLiveUpdates = ({ id }) => {
       });
     }
   }, []);
+
+  useEffect(() => {
+    if (!order) {
+      return;
+    }
+    const subscription = DataStore.observe(Order, order.id).subscribe((msg) => {
+      if (msg.opType === "UPDATE") {
+        setOrder(msg.element);
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, [order]);
+
   useEffect(() => {
     if (order?.driverID && order?.driverID != "null") {
       DataStore.query(Driver, order.driverID).then(setDriver);
     }
-  }, [order]);
+  }, [order?.driverID]);
   useEffect(() => {
     if (driver?.lng && driver?.lat) {
       mapRef.current.animateToRegion({
@@ -53,11 +69,18 @@ const OrderLiveUpdates = ({ id }) => {
   return (
     <View>
       <Text>Status: {order?.status || "loading"}</Text>
-      <MapView style={styles.map} ref={mapRef}>
+      <MapView style={styles.map} ref={mapRef} showsUserLocation>
         {driver?.lat && (
           <Marker coordinate={{ latitude: driver.lat, longitude: driver.lng }}>
             <View style={styles.driverIconContainer}>
               <FontAwesome5 name="motorcycle" size={24} color="white" />
+            </View>
+          </Marker>
+        )}
+        {dbUser && (
+          <Marker coordinate={{ latitude: dbUser.lat, longitude: dbUser.lng }}>
+            <View style={styles.driverIconContainer}>
+              <MaterialIcons name="restaurant" size={24} color="white" />
             </View>
           </Marker>
         )}
