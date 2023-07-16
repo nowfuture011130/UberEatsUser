bea45864eb2a782e12696065ea830d45b197657b
nowfuture011130 <105948127+nowfuture011130@users.noreply.github.com>
2023-07-16 00:20:17 -0400

    update

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -5,6 +5,8 @@ import {
   StyleSheet,
   Pressable,
   Alert,
+  KeyboardAvoidingView,
+  Button,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -13,15 +15,25 @@ import { DataStore } from "@aws-amplify/datastore";
 import { User2 } from "../../models";
 import { useAuthContext } from "../../contexts/AuthContext";
 import { useNavigation } from "@react-navigation/native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const Profile = () => {
   const { sub, setDbuser, dbUser } = useAuthContext();
 
   const [name, setName] = useState(dbUser ? dbUser.name : "");
   const [address, setAddress] = useState(dbUser ? dbUser.address : "");
+  const [googleAddress, setGoogleAddress] = useState(null);
   const [lat, setLat] = useState(dbUser ? dbUser.lat + "" : "0");
   const [lng, setLng] = useState(dbUser ? dbUser.lng + "" : "0");
+
   const navigation = useNavigation();
+
+  const getAddressLatLng = async (userAddress, details) => {
+    setGoogleAddress(userAddress);
+    setAddress(userAddress.description);
+    setLat(details?.geometry?.location.lat);
+    setLng(details?.geometry?.location.lng);
+  };
 
   useEffect(() => {
     const update = DataStore.observeQuery(User2, (c) =>
@@ -75,7 +87,7 @@ const Profile = () => {
     Auth.signOut();
   };
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <Text style={styles.title}>Profile</Text>
       <TextInput
         value={name}
@@ -83,59 +95,93 @@ const Profile = () => {
         placeholder="Name"
         style={styles.input}
       />
-      <TextInput
-        value={address}
-        onChangeText={setAddress}
-        placeholder="Address"
-        style={styles.input}
+      <View style={{ paddingLeft: 15 }}>
+        <Text style={{ fontSize: 20, fontWeight: "500" }}>
+          Current Address:
+        </Text>
+        <Text style={{ fontSize: 16, fontWeight: "400", paddingTop: 10 }}>
+          {address}
+        </Text>
+      </View>
+      <GooglePlacesAutocomplete
+        styles={{
+          textInputContainer: {
+            padding: 10,
+          },
+          textInput: {
+            height: 50,
+          },
+        }}
+        GooglePlacesDetailsQuery={{ fields: "geometry" }}
+        fetchDetails={true}
+        placeholder="Search your address here"
+        onPress={(data, details = null) => {
+          getAddressLatLng(data, details);
+        }}
+        query={{
+          key: "AIzaSyD0g5cZwzDSDdWGZ7qdU1pxooPTgUriE3M",
+          language: "en",
+        }}
       />
-      <TextInput
-        value={lat}
-        onChangeText={setLat}
-        placeholder="Latitude"
-        style={styles.input}
-        keyboardType="numeric"
-      />
-      <TextInput
-        value={lng}
-        onChangeText={setLng}
-        placeholder="Longitude"
-        style={styles.input}
-      />
-      <Pressable
-        onPress={onSave}
-        style={({ pressed }) => [
-          { backgroundColor: pressed ? "blue" : "cornflowerblue", margin: 20 },
-        ]}
+      {/* <GooglePlacesAutocomplete
+        apiKey="AIzaSyD0g5cZwzDSDdWGZ7qdU1pxooPTgUriE3M"
+        selectProps={{ value: googleAddress, onChange: getAddressLatLng }}
+      /> */}
+
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        <Text
+        <View
           style={{
-            textAlign: "center",
-            color: "white",
-            margin: 10,
-            fontSize: 18,
-            fontWeight: "bold",
+            width: "100%",
+            height: "80%",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "absolute",
+            bottom: 0,
           }}
         >
-          Save
-        </Text>
-      </Pressable>
-      <Pressable
-        onPress={signoutPress}
-        style={{ backgroundColor: "lightgray", margin: 20 }}
-      >
-        <Text
-          style={{
-            textAlign: "center",
-            color: "red",
-            margin: 10,
-            fontSize: 18,
-            fontWeight: "bold",
-          }}
-        >
-          Sign Out
-        </Text>
-      </Pressable>
+          <Pressable
+            onPress={onSave}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed ? "blue" : "cornflowerblue",
+                margin: 20,
+                width: "80%",
+              },
+            ]}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                margin: 10,
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+            >
+              Save
+            </Text>
+          </Pressable>
+          <Pressable
+            onPress={signoutPress}
+            style={{ backgroundColor: "lightgray", margin: 20, width: "80%" }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "red",
+                margin: 10,
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+            >
+              Sign Out
+            </Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
